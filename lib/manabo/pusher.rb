ee0be6db6bb4f7051e4e49fcb23ed6c2218412d9
Daisuke Yamashita <dddaisuke@gmail.com>
2015-02-23 15:16:29 +0900

    refactoring

--- lib/manabo/pusher.rb
+++ lib/manabo/pusher.rb
@@ -2,22 +2,29 @@ require "manabo/pusher/version"
 
 module Manabo
   module Pusher
-    def bulk_trigger(user_ids, key, value)
-      PusherLog.enqueue(user_ids: user_ids, key: key, message: value)
+    def bulk_trigger(user_ids, key, message)
+      PusherLog.enqueue(user_ids: user_ids, key: key, message: message)
       user_ids.each_slice(100).each do |user_ids|
-        Pusher.trigger(user_ids.map(&:to_s), key, value)
+        Pusher.trigger(user_ids.map(&:to_s), key, message)
       end
     end
 
-    def each_trigger(user_ids)
-      pusher_log_params = { user_ids: user_ids }.merge!(yield :for_logging)
+    def each_trigger(user_ids, push_messages = [])
+      messages = []
+      push_messages.each do |push_message|
+        message = { user_ids: user_ids }.merge!(push_message)
+        PusherLog.enqueue(message)
+        messages << message
+      end
       user_ids.each_slice(100).each do |user_ids|
-        yield
+        messages.each do |message|
+          Pusher.trigger(user_ids.map(&:to_s), message[:key], message[:message])
+        end
       end
     end
 
-    def trigger(user_id, key, value)
-      Pusher[user_id].trigger(key, value)
+    def trigger(user_id, key, message)
+      Pusher[user_id].trigger(key, message)
     end
   end
 end
