3fcfef3cfd687c220c9b1c7fc8ac328d15f04a66
Daisuke Yamashita <dddaisuke@gmail.com>
2015-02-23 15:16:20 +0900

    delete worker generator

--- lib/generators/worker_generator.rb
+++
@@ -1,7 +0,0 @@
-class UploaderGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path("../templates", __FILE__)
-
-  def create_uploader_file
-    template "worker.rb", File.join('app/workers', class_path, 'pusher_log_worker.rb')
-  end
-end
